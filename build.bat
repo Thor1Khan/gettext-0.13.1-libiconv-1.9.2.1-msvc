3cf567083bcae7ca41baf6f3e433c0d844fb1b46
unknown <pbarthe@PC-PBA.PBA>
2012-12-08 22:50:52 +0100

    orig

---
+++ build.bat
@@ -0,0 +1,14 @@
+pushd libiconv-1.9.2-src
+nmake -f Makefile.msvc NO_NLS=1 MFLAGS=-MD
+nmake -f Makefile.msvc NO_NLS=1 MFLAGS=-MD install PREFIX=%~dp0gettext
+popd
+pushd gettext-0.13.1
+nmake -f Makefile.msvc MFLAGS=-MD PREFIX=%~dp0gettext
+nmake -f Makefile.msvc MFLAGS=-MD install PREFIX=%~dp0gettext
+popd
+pushd libiconv-1.9.2-src
+nmake -f Makefile.msvc NO_NLS=1 MFLAGS=-MD distclean
+nmake -f Makefile.msvc MFLAGS=-MD PREFIX=%~dp0gettext
+nmake -f Makefile.msvc MFLAGS=-MD install PREFIX=%~dp0gettext
+popd
+
