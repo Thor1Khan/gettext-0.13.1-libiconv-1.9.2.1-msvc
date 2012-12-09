26836cea2f9fe87ed9bf26495e71a367e34bbe2a
unknown <pbarthe@PC-PBA.PBA>
2012-12-09 06:16:50 +0100

    .

--- build.bat
+++ build.bat
@@ -1,15 +1,15 @@
 if not exist %~dp0gettext mkdir %~dp0gettext
 pushd libiconv-1.9.2-src
-nmake -f Makefile.msvc DLL=1 NO_NLS=1 MFLAGS=-MD INSTALLPREFIX=%~dp0gettext
-nmake -f Makefile.msvc DLL=1 NO_NLS=1 MFLAGS=-MD install INSTALLPREFIX=%~dp0gettext
+nmake -f Makefile.msvc DLL=1 NO_NLS=1 MFLAGS=-MD
+nmake -f Makefile.msvc DLL=1 NO_NLS=1 MFLAGS=-MD install PREFIX=%~dp0gettext
 popd
 pushd gettext-0.13.1
-nmake -f Makefile.msvc DLL=1 MFLAGS=-MD INSTALLPREFIX=%~dp0gettext
-nmake -f Makefile.msvc DLL=1 MFLAGS=-MD install INSTALLPREFIX=%~dp0gettext
+nmake -f Makefile.msvc DLL=1 MFLAGS=-MD PREFIX=%~dp0gettext
+nmake -f Makefile.msvc DLL=1 MFLAGS=-MD install PREFIX=%~dp0gettext
 popd
 pushd libiconv-1.9.2-src
-nmake -f Makefile.msvc DLL=1 NO_NLS=1 MFLAGS=-MD INSTALLPREFIX=%~dp0gettext distclean
-nmake -f Makefile.msvc DLL=1 MFLAGS=-MD INSTALLPREFIX=%~dp0gettext
-nmake -f Makefile.msvc DLL=1 MFLAGS=-MD install INSTALLPREFIX=%~dp0gettext
+nmake -f Makefile.msvc DLL=1 NO_NLS=1 MFLAGS=-MD distclean
+nmake -f Makefile.msvc DLL=1 MFLAGS=-MD PREFIX=%~dp0gettext
+nmake -f Makefile.msvc DLL=1 MFLAGS=-MD install PREFIX=%~dp0gettext
 popd
 
